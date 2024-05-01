eb1e4ef7387d50d16404d17a97ffc699bff9b3a8
JuanAzula <141027910+JuanAzula@users.noreply.github.com>
2024-05-01 18:36:03 +0200

    Fix text

--- src/const.ts
+++ src/const.ts
@@ -1,18 +1,18 @@
 export const scaleFactor = 4;
 
 export const dialogueData: Record<string, string> = {
-    pc: `This is my PC. I work mostly in Full Stack Projects these days.
+  pc: `This is my PC. I work mostly in Full Stack Projects these days.
     Here is my <a href="https://github.com/juanazula" target="_blank">Github</a>!`,
-    "cs-degree": `Master of Software Development from AIT, extensive certifications in JavaScript, React, Angular, Java, HTML/CSS/JavaScript, Big Data, and Business Intelligence, alongside proficiency in Python, Node.js, frontend and backend frameworks, and agile methodologies.`,
-    "sofa-table": `That's my sofa. I like to relax here.
+  "cs-degree": `Master of Software Development from AIT, extensive certifications in JavaScript, React, Angular, Java, HTML/CSS/JavaScript, Big Data, and Business Intelligence, alongside proficiency in Python, Node.js, frontend and backend frameworks, and agile methodologies.`,
+  "sofa-table": `That's my sofa. I like to relax here.
     On the table it is my resume. <a href="https://docs.google.com/document/d/e/2PACX-1vQ3DTtHsstBEw82DGMdzXubabr5UrOnwTVErDBf3SaKD4USjq1qpYHw7XUldMZQJxaYUQYfsiaMNwfS/pub" target="_blank">Check it out?</a>
     Contact me at juanazula3@gmail.com if you have any interesting job opportunities!`,
-    tv: `That's my TV. I've been watching tech youtubers a lot recently like :
+  tv: `That's my TV. I've been watching tech youtubers a lot recently like :
    <a href="https://www.youtube.com/@Midulive" target="_blank">Midudev</a>, <a href="https://www.youtube.com/@JavaScriptMastery" target="_blank">JavaScript Mastery</a>,
   <a href="https://www.youtube.com/@CodelyTV" target="_blank">CodelyTV</a> (sometimes) and <a href="https://www.youtube.com/@CodeWithAntonio" target="_blank">Code With Antonio</a>!`,
-    bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-    resume: `I've made a couple of projects with different technologies. The one I'm most proud of is <a href="https://github.com/juanazula/vibesync" target="_blank">this one</a>(the backend is on another repo)`,
-    projects: `I've made a couple of projects with different technologies. The one I'm most proud of is <a href="https://github.com/juanazula/vibesync" target="_blank">this one</a>(the backend is on another repo)`,
-    library: `There are a lot of programming books on my shelves. There is even one in German (I also speak German btw).`,
-    exit: `If you want to exit Juan Azula's portfolio, just close the tab.`,
+  bed: `This is where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `I've made a couple of projects with different technologies. The one I'm most proud of is <a href="https://github.com/juanazula/vibesync" target="_blank">this one</a>(the backend is on another repo)`,
+  projects: `I've made a couple of projects with different technologies. The one I'm most proud of is <a href="https://github.com/juanazula/vibesync" target="_blank">this one</a>(the backend is on another repo)`,
+  library: `There are a lot of programming books on my shelves. There is even one in German (I also speak German btw).`,
+  exit: `If you want to exit Juan Azula's portfolio, just close the tab.`,
 };
